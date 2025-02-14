df70a3d3f1b703f8d14bbf96471d61b9c1e210c6
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-14 13:23:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+// } Driver Code Ends
+//User function Template for Java
+
+public static void utility(int number){
+    //Write your if statement below
+    if(number>100)System.out.println("Big");
+    
+    //Write your if statement above
+    System.out.println("Number");
+}
+
+//{ Driver Code Starts.
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            int number = scn.nextInt();
+            utility(number);
+        
+System.out.println("~");
+}
+        scn.close();
+    }
+}
+// } Driver Code Ends
